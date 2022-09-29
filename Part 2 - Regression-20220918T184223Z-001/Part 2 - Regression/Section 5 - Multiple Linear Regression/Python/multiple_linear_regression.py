feaedf8ab530f49d2ee9ac2a877a86f2cf9a4aa9
Tony Ly <tonyly936@gmail.com>
2022-09-29 19:13:19 -0400

    Section 7 - 59

--- Part 2 - Regression-20220918T184223Z-001/Part 2 - Regression/Section 5 - Multiple Linear Regression/Python/multiple_linear_regression.py
+++ Part 2 - Regression-20220918T184223Z-001/Part 2 - Regression/Section 5 - Multiple Linear Regression/Python/multiple_linear_regression.py
@@ -18,4 +18,11 @@ from sklearn.preprocessing import OneHotEncoder
 ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [3])], remainder='passthrough')
 X = np.array(ct.fit_transform(X))
 
-print(X)
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+
