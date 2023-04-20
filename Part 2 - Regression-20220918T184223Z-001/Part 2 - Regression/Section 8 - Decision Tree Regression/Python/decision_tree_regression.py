7e95f724f8a0fb30eb5b94b41e0545e84b6bbd1f
Tony Ly <73499945+TonyLy-Git@users.noreply.github.com>
2023-04-19 21:09:23 -0400

    rfr

--- Part 2 - Regression-20220918T184223Z-001/Part 2 - Regression/Section 8 - Decision Tree Regression/Python/decision_tree_regression.py
+++ Part 2 - Regression-20220918T184223Z-001/Part 2 - Regression/Section 8 - Decision Tree Regression/Python/decision_tree_regression.py
@@ -21,6 +21,14 @@ dtr = DecisionTreeRegressor()
 dtr.fit(X, y)
 
 # predicting new result
-print(dtr.predict([[6.5]]))
+dtr.predict([[6.5]])
 
 # visualizing results (high res)
+X_grid = np.arange(min(X), max(X), 0.1)
+X_grid = X_grid.reshape((len(X_grid), 1))
+plt.scatter(X, y, color='red')
+plt.plot(X_grid, dtr.predict(X_grid), color='blue')
+plt.title('DTR (High Res)')
+plt.xlabel('Position Level')
+plt.ylabel('Salary')
+plt.show()
---
+++ Part 2 - Regression-20220918T184223Z-001/Part 2 - Regression/Section 9 - Random Forest Regression/Python/random_forest_regression.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 19 20:37:42 2023
+
+@author: lyt4
+"""
+
