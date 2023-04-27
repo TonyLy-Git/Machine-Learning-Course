a315b1431044e6b5dbfaa2c19b95e88dd990594b
TonyLy-Git <73499945+TonyLy-Git@users.noreply.github.com>
2023-04-27 15:46:31 -0400

    logistical

--- Part 2 - Regression-20220918T184223Z-001/Part 2 - Regression/Section 13 - Regression Model Selection/Regression/multiple_linear_regression.py
+++ Part 2 - Regression-20220918T184223Z-001/Part 2 - Regression/Section 13 - Regression Model Selection/Regression/multiple_linear_regression.py
@@ -22,9 +22,9 @@ regressor.fit(X_train, y_train)
 # Predicting the Test set results
 y_pred = regressor.predict(X_test)
 np.set_printoptions(precision=2)
-print(np.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),1)),1))
+#print(np.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),1)),1))
 
 # Evaluating the Model Performance
 from sklearn.metrics import r2_score
 score = r2_score(y_test, y_pred)
-print(score)
+print("Mult: " + str(score))
